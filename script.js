9b1969331513cc4ba323c5634151a92085bb2e7d
natiaker <n.kerdzaia@gmail.com>
2022-10-24 12:43:19 +0400

    fix: update tip inputs

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let bill = document.querySelector(".input-section__bill");
 let people = document.querySelector(".input-section__people");
+let wrapper = document.querySelector(".input-section__tip");
 let tipBtn = document.querySelectorAll(".input-section__tip--btn");
 let tipInput = document.querySelector(".input-section__tip--input");
 let reset = document.querySelector(".result-section__reset");
@@ -24,7 +25,7 @@ function getInputValue(input) {
     return res;
 }
 
-function check_error(input, inputError = null) {
+function error_message(input, inputError = null) {
     if(input.value <= 0) {
        if(inputError != null) inputError.style.display = "inline";
         input.style.border = "2px solid #E17052";
@@ -41,6 +42,10 @@ function calculate() {
     let tipValue = getInputValue(tipInput);
     let peopleValue = getInputValue(people);
 
+    if (isButton) { 
+        tipValue = tipRate; 
+    } else { prevButton.style.backgroundColor = "#00474B"; }
+
     let tip = calculate_tip(billValue, tipValue, peopleValue);
     let total = calculate_total(billValue, tipValue, peopleValue);
     tipAmountResult.innerText = `$${tip.toFixed(2)}`;
@@ -49,7 +54,7 @@ function calculate() {
 
 function eventListenerFunc(element, elementError) {
     element.addEventListener("input", () => { 
-        if (check_error(element, elementError) ) { 
+        if (error_message(element, elementError) ) { 
             reset.style.backgroundColor = "#26C2AE";
             reset.style.cursor = "pointer";
             reset.disabled = false;
@@ -65,16 +70,37 @@ eventListenerFunc(bill, billError);
 eventListenerFunc(tipInput);
 eventListenerFunc(people, peopleError);
 
-tipBtn.forEach(el => {
-    el.onclick = (event) => {
-        tipRate = parseInt(event.target.textContent);
-        tipInput.value = tipRate;
+// tipBtn.forEach(el => {
+//     el.onclick = (event) => {
+//         tipRate = parseInt(event.target.textContent);
+//         tipInput.value = tipRate;
 
-        reset.style.backgroundColor = "#26C2AE";
-        reset.style.cursor = "pointer";
-        reset.disabled = false;
-    }
-})
+//         reset.style.backgroundColor = "#26C2AE";
+//         reset.style.cursor = "pointer";
+//         reset.disabled = false;
+//     }
+// })
+
+let isButton = null;
+let prevButton = null;
+wrapper.addEventListener('click', (e) => {
+  isButton = e.target.nodeName === 'BUTTON'; 
+  if (!isButton) return;
+
+  e.target.style.backgroundColor = "#26C2AE";
+
+  tipRate = parseInt(e.target.textContent);
+  calculate();
+
+  reset.style.backgroundColor = "#26C2AE";
+  reset.style.cursor = "pointer";
+  reset.disabled = false;
+
+  if(prevButton !== null) {
+    prevButton.style.backgroundColor = "#00474B";  
+  }
+  prevButton = e.target;
+});
 
 reset.addEventListener("click", () => {
     let inputs = [bill, people, tipInput];
@@ -82,7 +108,8 @@ reset.addEventListener("click", () => {
         input.value = "";
         input.style.border = "2px solid hsla(185, 38%, 97%, 1)";
     }
-
+    
+    prevButton.style.backgroundColor = "#00474B"; 
     reset.disabled = true;
     reset.style.cursor = "unset";
     reset.style.backgroundColor = "#0D686D";
